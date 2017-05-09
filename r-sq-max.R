6c3f3c13ff8f9533e33dd9bed8a693737cd67c3c
vince.forgetta <forgetta@gmail.com>
2017-05-09 09:12:30 -0400

    Cosmetic changes to r-sq-max.R

--- r-sq-max.R
+++ r-sq-max.R
@@ -21,8 +21,10 @@ y <- seq(0.005, 0.5, 0.0005)
 g <- expand.grid(x,y)
 g$r2max <- apply(expand.grid(x,y),1,"getr2max")
 
-ix <- log(x,10)
-iy <- log(y,10)
+lf <- 10
+
+ix <- log(x,lf)
+iy <- log(y,lf)
 
 png(file="r-sq-max.png", width = 2400, height=2000, res=360)
 lv=seq(0,1,0.05)
@@ -33,13 +35,13 @@ filled.contour(ix,iy, matrix(g$r2max, nrow = length(x), ncol=length(y), byrow=TR
                xlab="% MAF(a)",
                ylab="% MAF(b)",
                plot.axes = {
-                 abline(h=log(matrix(seq(0.0001,0.001,0.0001) %*% t(c(1,10,100,1000)))[1:35],10), col = "gray", lty = "dotted")
-                 abline(v=log(matrix(seq(0.0001,0.001,0.0001) %*% t(c(1,10,100,1000)))[1:35],10), col = "gray", lty = "dotted")
+                 abline(h=log(matrix(seq(0.0001,0.001,0.0001) %*% t(c(1,10,100,1000)))[1:35],lf), col = "gray", lty = "dotted")
+                 abline(v=log(matrix(seq(0.0001,0.001,0.0001) %*% t(c(1,10,100,1000)))[1:35],lf), col = "gray", lty = "dotted")
                  contour(ix,iy, matrix(g$r2max, nrow = length(x), ncol=length(y), byrow=TRUE), 
                          levels=clv, add=TRUE, labcex = 0.6,  lty="dotted")
                  abline(a=0,b=1, col=rev(cm.colors(length(lv)))[1], lwd=2)
-                 axis(1, at=log(labels,10), labels = labels*100)
-                 axis(2, at=log(labels,10), labels = labels*100, las=1)
+                 axis(1, at=log(labels,lf), labels = labels*100)
+                 axis(2, at=log(labels,lf), labels = labels*100, las=1)
                  
                  }
                )
